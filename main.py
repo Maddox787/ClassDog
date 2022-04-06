d79a7a4a22530f6fcb58191969fbb3a336dff541
Maddox787 <102323418+Maddox787@users.noreply.github.com>
2022-04-06 18:52:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+ClassDog:
+def __innit__(self.color,fur,legs,body,gender)
+
+self.color = tan
+self.fur = soft and fluffy 
+self.legs = short
+self.body = chubby and long
+self.gender = girl
+
+
---
+++ person .py
@@ -0,0 +1,9 @@
+ClassPerson: 
+
+def __innit__(self._skincolor,_height,_eyecolor,_voice,_gend-er,_name)
+  self.skincolor = _tan   
+  self.height = _5'5
+  self.eyecolor = _darkbrown
+  self.voice = _deep-ish
+  self.gender = _male
+  self.name = _Maddox
